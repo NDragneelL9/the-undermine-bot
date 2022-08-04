dda2684166f42653380297ddb425d8098aaab68e
NDragneelL9 <nell.dragneel@gmail.com>
2022-08-04 12:21:54 +0300

    bugfix/Fix telegram bot specs

--- spec/requests/telegram_webhooks_controller_spec.rb
+++ spec/requests/telegram_webhooks_controller_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
+require 'telegram/bot/rspec/integration/rails'
 
-RSpec.describe "TelegramWebhooksControllers", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe TelegramWebhooksController, telegram_bot: :rails do
+
+  describe '#start!' do
+    subject { -> { dispatch_command :start } }
+    it { should respond_with_message I18n.t('telegram_webhooks.start.content',  first_name: "") }
+  end
+
+  describe '#wow_token!' do
+    subject { -> { dispatch_command :wow_token } }
+    it { should respond_with_message 'Calling blizzard...' }
   end
 end
