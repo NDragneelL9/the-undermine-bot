9a3b35caa6f26ccc3bd0363e09d39faf63304682
Tim <70818761+NDragneelL9@users.noreply.github.com>
2023-04-21 16:25:47 +0300

    update ruby ver and gems

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Main class to work with application mailers
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Main class to work with application records
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
